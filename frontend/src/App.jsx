a81b682573d01a7956c15329eaebdbf1723b4ebe
Zior <zior.ered1@gmail.com>
2023-09-06 01:55:24 -0700

    header create

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+// import { Link } from 'react-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        <div>
+          List group
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
+
+
