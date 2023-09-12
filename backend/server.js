e3f7ae7f5ef09f1b30d059875a2f8f015dbfb4ee
Zior <zior.ered1@gmail.com>
2023-09-12 03:40:40 -0700

    Complete-Add-To-Cart

--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,13 @@ app.get("/api/products/slug/:slug", (req, res) => {
   else res.status(404).send({ message: "Product Not Found" });
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
